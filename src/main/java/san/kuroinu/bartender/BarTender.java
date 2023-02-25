30e0e0be85147d83a59537fb28002a57b53f3f6a
黒狗さん。 <kuroinusanshinji@gmail.com>
2023-02-25 21:28:04 +0900

    完成

---
+++ src/main/java/san/kuroinu/bartender/BarTender.java
@@ -0,0 +1,50 @@
+package san.kuroinu.bartender;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BarTender extends JavaPlugin {
+    public static JavaPlugin plugin;
+    private Listeners listeners;
+    public static String prefix = ChatColor.DARK_PURPLE +"[BarTender]"+ChatColor.RESET;
+    public static Economy econ = null;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        plugin = this;
+        this.listeners = new Listeners();
+        getServer().getPluginManager().registerEvents(this.listeners, this);
+        super.onEnable();
+        if (!setupEconomy() ) {
+            getServer().getConsoleSender().sendMessage(prefix + " Vault not found, disabling plugin.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        plugin.saveDefaultConfig();
+    }
+
+    private static Boolean setupEconomy() {
+        if (getPlugin().getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getPlugin().getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null){
+            return false;
+        }else{
+            econ = rsp.getProvider();
+        }
+        return econ != null;
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    private static JavaPlugin getPlugin() {
+        return plugin;
+    }
+}
